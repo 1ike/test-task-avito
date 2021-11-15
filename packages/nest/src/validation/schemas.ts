07d5f4d527c5f2f70ec56bab61fb06a9bd984cfe
1ike <googlohren@gmail.com>
2021-11-15 17:29:27 +0300

    Make Joi validation work.

---
+++ packages/nest/src/validation/schemas.ts
@@ -0,0 +1,35 @@
+import * as Joi from 'joi';
+
+const baseIdSchema = Joi.number().integer();
+
+const baseEntitySchema = Joi.object({
+  id: baseIdSchema.required(),
+  by: Joi.string().required(),
+  time: Joi.date().timestamp('javascript').required(),
+  kids: Joi.array().items(Joi.number().integer()),
+  deleted: Joi.boolean(),
+}).unknown();
+
+const storySchema = baseEntitySchema
+  .append({
+    title: Joi.string().required(),
+    score: Joi.number().integer(),
+    url: Joi.string().uri(),
+    text: Joi.string(),
+  })
+  .xor('url', 'text');
+
+const commentSchema = baseEntitySchema.append({
+  text: Joi.string().required(),
+  parent: baseIdSchema,
+});
+
+export enum ValidationEntities {
+  Story = 'Story',
+  Comment = 'Comment',
+}
+
+export const schemas = {
+  [ValidationEntities.Story]: storySchema,
+  [ValidationEntities.Comment]: commentSchema,
+};
--- packages/nest/src/validation/validation.service.ts
+++ packages/nest/src/validation/validation.service.ts
@@ -1,44 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import * as Joi from 'joi';
-
-const baseIdSchema = Joi.number().integer();
-const baseEntitySchema = Joi.object({
-  id: baseIdSchema.required(),
-  by: Joi.string().required(),
-  time: Joi.date().timestamp('javascript').required(),
-  kids: Joi.array().items(Joi.number().integer()),
-  deleted: Joi.boolean(),
-});
-
-const storySchema = baseEntitySchema
-  .append({
-    title: Joi.string().required(),
-    score: Joi.number().integer(),
-    url: Joi.string().uri(),
-    text: Joi.string(),
-  })
-  .xor('url', 'text');
-
-const commentSchema = baseEntitySchema.append({
-  text: Joi.string().required(),
-  parent: baseIdSchema,
-});
-
-enum ValidationEntities {
-  Story = 'Story',
-  Comment = 'Comment',
-}
-
-const schemas = {
-  [ValidationEntities.Story]: storySchema,
-  [ValidationEntities.Comment]: commentSchema,
-};
+import { ValidationEntities, schemas } from './schemas';
 
 @Injectable()
 export class ValidationService {
   validateEntity(entity, name = ValidationEntities.Story) {
     if (!Boolean(entity)) return false;
 
-    return schemas[name].validate(entity);
+    return !Boolean(schemas[name].validate(entity).error);
   }
 }
