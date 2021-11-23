62fd64e86fddc622841c5bd81b59aba2b8eefa00
1ike <googlohren@gmail.com>
2021-11-23 23:13:29 +0300

    Add env variables.

--- packages/angular/src/environments/environment.ts
+++ packages/angular/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  HACKER_NEWS_API_URL: 'https://hacker-news.firebaseio.com/v0/',
+  POLLING_INTERVAL: 60000,
+  STORIES_QTY: 100,
+  STORIES_QTY_PER_PAGE: 10,
 };
 
 /*
