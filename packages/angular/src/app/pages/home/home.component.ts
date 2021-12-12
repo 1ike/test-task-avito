17d14bdef0788b610cdb3cc53e73e9edab51a9ef
1ike <googlohren@gmail.com>
2021-12-12 21:05:11 +0300

    Render stories at Home page.

--- packages/angular/src/app/pages/home/home.component.ts
+++ packages/angular/src/app/pages/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {  Observable,
+} from 'rxjs';
 
+import { StoryInterface, Time } from '@test-task-avito/shared';
 import { APIService } from '../../services/api.service';
+import { DateService } from '../../services/date.service';
+
 
 @Component({
   selector: 'app-home',
@@ -9,12 +14,13 @@ import { APIService } from '../../services/api.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private apiService: APIService) { }
+  stories$!: Observable<StoryInterface[]>;
+
+  constructor(private apiService: APIService, private dateService: DateService) { }
 
   ngOnInit(): void {
-    console.log(' Home ');
-
-    this.apiService.getNewestStories(10).subscribe(data => console.log('appService data = ', data));
+    this.stories$ = this.apiService.getNewestStories(10);
   }
 
+  formatDate = (date: Time) => this.dateService.formatDate(date);
 }
